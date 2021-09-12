a42715d50747511688d6cc48419f15453c9b3888
Erfan <erfan.pirikhah1@gmail.com>
2021-09-13 00:03:48 +0430

    test

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
-const express = require('express');
-const io = require('socket.io');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const User = require('./models/User');
+const express = require("express");
+const io = require("socket.io");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const User = require("./models/User");
 const app = express();
-const { uploadVoice, uploadFile } = require('./middleware/upload');
+const { uploadVoice, uploadFile } = require("./middleware/upload");
 app.use(bodyParser());
 app.use(cors());
-app.use(express.static('public'));
-app.get('/', (req, res) => {
-  res.send('salam . I am alive');
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.send("salam . I am alive");
 });
-app.post('/login', async (req, res) => {
+
+app.use("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
+});
+
+app.post("/login", async (req, res) => {
   const username = req.body.username;
   const gender = parseInt(req.body.gender);
   const user = await User.findOne({ username });
@@ -25,19 +30,19 @@ app.post('/login', async (req, res) => {
   res.status(200).send({});
 });
 
-app.get('/getUsers', async (req, res) => {
+app.get("/getUsers", async (req, res) => {
   const users = await User.find({});
   res.send(users);
 });
 
-const upVoice = uploadVoice.fields([{ name: 'voiceMessage', maxCount: 1 }]);
-const upFiles = uploadFile.fields([{ name: 'file', maxCount: 1 }]);
+const upVoice = uploadVoice.fields([{ name: "voiceMessage", maxCount: 1 }]);
+const upFiles = uploadFile.fields([{ name: "file", maxCount: 1 }]);
 
-app.post('/uploadVoice', upVoice, async (req, res) => {
+app.post("/uploadVoice", upVoice, async (req, res) => {
   if (req.files) {
     const filePath =
       `http://localhost:3010/` +
-      req.files.voiceMessage[0].path.slice(7).replace(/\\/g, '/');
+      req.files.voiceMessage[0].path.slice(7).replace(/\\/g, "/");
     await res.json({
       filePath: filePath,
     });
@@ -47,11 +52,11 @@ app.post('/uploadVoice', upVoice, async (req, res) => {
     });
   }
 });
-app.post('/uploadFile', upFiles, async (req, res) => {
+app.post("/uploadFile", upFiles, async (req, res) => {
   if (req.files) {
     const filePath =
       `http://localhost:3010/` +
-      req.files.file[0].path.slice(7).replace(/\\/g, '/');
+      req.files.file[0].path.slice(7).replace(/\\/g, "/");
     await res.json({
       filePath: filePath,
     });
@@ -63,72 +68,72 @@ app.post('/uploadFile', upFiles, async (req, res) => {
 });
 
 const server = app.listen(3010, (err) => {
-  console.log('App Listen to port 3010');
+  console.log("App Listen to port 3010");
 });
 
 mongoose
-  .connect('mongodb://localhost:27017/socketApp', {
+  .connect("mongodb://localhost:27017/socketApp", {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
   })
-  .then(() => console.log('Mongo connected...'))
-  .catch((err) => console.log('Could not connect to MongoDB...', err));
+  .then(() => console.log("Mongo connected..."))
+  .catch((err) => console.log("Could not connect to MongoDB...", err));
 
 const socket = io(server);
-const mySocket = socket.of('/socket');
+const mySocket = socket.of("/socket");
 
-mySocket.on('connection', (socket) => {
-  console.log('new User Connected');
-  socket.on('newMessage', (message) => {
+mySocket.on("connection", (socket) => {
+  console.log("new User Connected");
+  socket.on("newMessage", (message) => {
     const myUsername = message.sender.name;
     const username = message.receiver.name;
     mySocket
       .to(`${myUsername}:${username}`)
       .to(`${username}:${myUsername}`)
-      .emit('newMessage', {
+      .emit("newMessage", {
         ...message,
         date: new Date(),
         id: Math.floor(Math.random() * Math.pow(10, 7)),
       });
   });
-  socket.on('editMessage', ({ msg, id, sender, receiver }) => {
+  socket.on("editMessage", ({ msg, id, sender, receiver }) => {
     const myUsername = sender;
     const username = receiver;
     mySocket
       .to(`${myUsername}:${username}`)
       .to(`${username}:${myUsername}`)
-      .emit('editMsg', { msg, id });
+      .emit("editMsg", { msg, id });
   });
-  socket.on('deleteMsg', (id) => {
-    mySocket.emit('deleteMsg', id);
+  socket.on("deleteMsg", (id) => {
+    mySocket.emit("deleteMsg", id);
   });
-  socket.on('isTyping', ({ sender, receiver, isTyping }) => {
+  socket.on("isTyping", ({ sender, receiver, isTyping }) => {
     const myUsername = receiver;
     const username = sender;
     mySocket
       .to(`${myUsername}:${username}`)
       .to(`${username}:${myUsername}`)
-      .emit('isTyping', { username, isTyping });
+      .emit("isTyping", { username, isTyping });
   });
-  socket.on('seenMessage', ({ sender, receiver, id }) => {
-    console.log('seenMessage ', sender, receiver);
+  socket.on("seenMessage", ({ sender, receiver, id }) => {
+    console.log("seenMessage ", sender, receiver);
     const myUsername = receiver;
     const username = sender;
     mySocket
       .to(`${myUsername}:${username}`)
       .to(`${username}:${myUsername}`)
-      .emit('seenMessage', id);
+      .emit("seenMessage", id);
   });
-  socket.on('uploadVoice', ({ path, sender, receiver }) => {
+  socket.on("uploadVoice", ({ path, sender, receiver }) => {
     const myUsername = sender.name;
     const username = receiver.name;
     mySocket
       .to(`${myUsername}:${username}`)
       .to(`${username}:${myUsername}`)
-      .emit('newMessage', {
-        type: 'voice',
+      .emit("newMessage", {
+        type: "voice",
         sender,
         receiver,
         date: new Date(),
@@ -136,14 +141,14 @@ mySocket.on('connection', (socket) => {
         id: Math.floor(Math.random() * Math.pow(10, 7)),
       });
   });
-  socket.on('uploadFile', ({ path, sender, receiver }) => {
+  socket.on("uploadFile", ({ path, sender, receiver }) => {
     const myUsername = sender.name;
     const username = receiver.name;
     mySocket
       .to(`${myUsername}:${username}`)
       .to(`${username}:${myUsername}`)
-      .emit('newMessage', {
-        type: 'file',
+      .emit("newMessage", {
+        type: "file",
         sender,
         receiver,
         date: new Date(),
@@ -151,16 +156,16 @@ mySocket.on('connection', (socket) => {
         id: Math.floor(Math.random() * Math.pow(10, 7)),
       });
   });
-  socket.on('joinChat', ({ username, myUsername }) => {
+  socket.on("joinChat", ({ username, myUsername }) => {
     socket.join(`${username}:${myUsername}`);
     socket.join(`${myUsername}:${username}`);
   });
-  socket.on('leftChat', ({ username, myUsername }) => {
+  socket.on("leftChat", ({ username, myUsername }) => {
     socket.leave(`${username}:${myUsername}`);
     socket.leave(`${myUsername}:${username}`);
   });
 
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
   });
 });
